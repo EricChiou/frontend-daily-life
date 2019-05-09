386494bf870df70b2cc5bf7b3cac62daf2e98d26
EricChiou <miatlab@gmail.com>
2019-05-10 01:36:29 +0800

    modify about

--- src/views/about/About.vue
+++ src/views/about/About.vue
@@ -5,8 +5,8 @@
       前端工程師，平常喜歡玩電腦、看電影、寫寫自己的網站、玩貓！(´ω`)
       <div class="about-img">
         <img src="../../assets/images/play_cat.jpg">
-      </div>主修 Angular2，對於 React、Vue 也有涉略。
-      <br>略懂後端，主要使用過 Golang 和 Nodejs，也寫過 Java 和 Python。
+      </div>主修 Angular2，對於 React、Vue 也略有涉略。
+      <br>懂一些後端，主要使用過 Golang 和 Nodejs，也寫過 Java 和 Python。
       <br>
       <br>可是其實以前在研究所寫的是 c (遮臉)。
       <br>
