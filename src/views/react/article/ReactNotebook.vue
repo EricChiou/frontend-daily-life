4ba832a3e13529be83e3a070fc12caad38feb17b
EricChiou <miatlab@gmail.com>
2019-05-20 00:35:26 +0800

    update reactnotebook

--- src/views/react/article/ReactNotebook.vue
+++ src/views/react/article/ReactNotebook.vue
@@ -6,6 +6,27 @@
       <div class="summary">{{article.summary}}</div>
       <div class="under-line"></div>
       <div class="content">
+        <div class="sub-header">React 基本 Component</div>
+        <hr>
+        <div class="text">
+          <div class="code-area">
+            import React, { Component } from 'react';
+            <br>
+            <br>class Component extends Component {
+            <br>&nbsp;&nbsp;render() {
+            <br>&nbsp;&nbsp;&nbsp;&nbsp;return (
+            <br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lt;div&gt;&lt;/div&gt;
+            <br>&nbsp;&nbsp;&nbsp;&nbsp;);
+            <br>&nbsp;&nbsp;}
+            <br>}
+            <br>
+            <br>export default Component;
+            <div class="copy" @click="copy">
+              <img src="../../../assets/images/copy_icon.svg">
+            </div>
+          </div>
+          <br>
+        </div>
         <div class="sub-header">React Lifecycle Hooks</div>
         <hr>
         <div class="text">
@@ -18,8 +39,21 @@
 
 <script>
 import Cons from '../../../util/constants';
+import Clipboard from '../../../util/clipboard';
 
 export default {
-  data: () => ({ article: Cons.article.reactNotebook })
+  data: () => ({ article: Cons.article.reactNotebook }),
+  methods: {
+    copy(event) {
+      let ele = event.target.parentElement;
+      while (ele) {
+        if (ele.className === 'code-area') {
+          Clipboard.copy2Clipboard(ele);
+          break;
+        }
+        ele = ele.parentElement;
+      }
+    }
+  }
 };
 </script>
