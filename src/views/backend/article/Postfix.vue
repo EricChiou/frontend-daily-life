e1baeca64db234fd0443127dcecf0e8074efce0c
EricChiou <miatlab@gmail.com>
2021-02-25 20:00:27 +0800

    update postfix article

--- src/views/backend/article/Postfix.vue
+++ src/views/backend/article/Postfix.vue
@@ -38,6 +38,28 @@
         <br />
         <div class="sub-header">Internet protocols to use</div>
         <CodeBlock :copy="false">選 all</CodeBlock>
+        <br />
+        <div class="header">本地測試</div>
+        <hr />
+        輸入 "telnet localhost 25" 與 Postfix 進行連線
+        <CodeBlock>telnet localhost 25</CodeBlock>
+        <br />
+        設定寄件者信箱
+        <CodeBlock>mail from: system@mail.com</CodeBlock>
+        設定收件者信箱
+        <CodeBlock>rcpt to: user@mail.com</CodeBlock>
+        <br />
+        輸入 "data" 開始寫入信件內容
+        <CodeBlock>data</CodeBlock>
+        信件標題
+        <CodeBlock>Subject: Test send mail</CodeBlock>
+        信件內容
+        <CodeBlock>Hello, xxx.</CodeBlock>
+        最後單獨一行輸入 "." 結束寫入信件
+        <CodeBlock>.</CodeBlock>
+        <br />
+        成功之後會看到下面類似指令
+        <CodeBlock>250 2.0.0 Ok: queued as DCF05E80063B</CodeBlock>
       </div>
     </div>
   </div>
